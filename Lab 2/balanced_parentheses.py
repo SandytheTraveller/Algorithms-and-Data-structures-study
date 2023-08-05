8b83c9196770e8db350550a31df82fc96f0f74f9
SandytheTraaveller <alesksandrak@gmail.com>
2023-08-05 18:53:53 +0200

    lecture 2

---
+++ Lab 2/balanced_parentheses.py
@@ -0,0 +1,32 @@
+# check if a string contains a balanced number of parentheses
+from stack import Stack
+
+openers = ["(", "[", "{"]
+closers = [")", "]", "}"]
+
+
+def matches(open, close):
+    return openers.index(open) == closers.index(close)
+def parentheses_checker(symbols_string):
+    s = Stack()
+
+    for symbol in symbols_string:
+        # if the current symbol is an opening parenthesis,
+        # it is pushed in the stack
+        if symbol in openers:
+            s.push(symbol)
+        else:
+            if s.isEmpty():
+                return False
+            else:
+                # if the current symbol is a closing parenthesis, check if the element
+                # on top of the stack is an opening of the correct type
+                if symbol in closers:
+                    top = s.pop()
+                    if not matches(top, symbol):
+                        return False
+    if s.isEmpty():
+        return True
+
+t = "( [ ] { } )"
+print(parentheses_checker(t))
